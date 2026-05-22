a22475417aabf400ec75536a46714f4f9158bfed
David <david.tuaillon68@gmail.com>
2026-05-22 16:55:14 +0200

    ajout du gitignore

--- include/equipement.h
+++ include/equipement.h
@@ -16,16 +16,27 @@
 #define TRAME_BOURRAGE_TAILLE 4
 
 typedef struct station {
-    int8_t addrMac[MAC_TAILLE];
+    int8_t addr_mac[MAC_TAILLE];
     int8_t ip[IPV4_TAILLE];
 } station;
 
 typedef struct switch_ {
     int8_t addrMac[MAC_TAILLE];
     size_t nb_ports;
+    size_t priorite;
+    int32_t* table_commutation;
+    size_t nb_equipement;
+    size_t max_equipement;
+
 } switch_;
+
+typedef struct reseau_local {
+    
+} reseau_local;
+
 
 typedef struct trame {
 
-
 } trame;
+
+
