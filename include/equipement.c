4b71a3f6efe9c4e07cdab75f947755c5c26a2fd8
David <david.tuaillon@etu.unistra.fr>
2026-05-28 09:15:21 +0200

    ajouts des attributs des structs

---
+++ include/equipement.c
@@ -0,0 +1,6 @@
+#include "equipement.h"
+
+Erreur_fichier charger_reseau(char* fichier, reseau_local *r)
+{
+    FILE* fichier = fopen();
+}
--- include/equipement.h
+++ include/equipement.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAC_TAILLE 48
 #define IPV4_TAILLE 32
@@ -27,10 +28,12 @@ typedef struct station {
   MAC mac;
   IPV4 ipv4;
 } station;
+
 typedef struct table_de_commutation {
   MAC mac;
   size_t interface_port;
 } table_de_commutation;
+
 typedef struct switch_ {
   MAC mac;
   size_t nb_port;
@@ -41,10 +44,45 @@ typedef struct switch_ {
 
 } switch_;
 
+typedef struct cable {
+  MAC sommet1;
+  MAC sommet2;
+  size_t ponderation;
+} cable;
+
+typedef enum type_equipement {
+	STATION,
+	SWITCH
+
+} type_equipement;
+
+typedef struct equipement {
+	type_equipement type_equ;
+	union {	
+		station st;
+		switch_ sw;
+	}; //equipement est soi une station soit un switch
+} equipement;
+
 typedef struct reseau_local {
+	size_t ordre;
+	equipement* equipements;
+
+	size_t nb_equipements_reseau;
+	cable* cables;
+	size_t nb_cables;
 
 } reseau_local;
 
 typedef struct trame {
 
 } trame;
+
+typedef enum Erreur_fichier
+{
+  ERR_FICHIER_NON_TROUVE,
+  ERR_OUVERTURE_IMPOSSIBLE,
+  ERR_INCONNU
+} Erreur_fichier;
+
+Erreur_fichier charger_reseau(char* fichier, reseau_local *r);
