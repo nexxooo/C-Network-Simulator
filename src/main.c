7a220d5e9ff462146bda32cb6404a75bfa6f6f39
David <david.tuaillon68@gmail.com>
2026-06-10 13:47:40 +0200

    petit fix suppr truc dans main

--- src/main.c
+++ src/main.c
@@ -4,11 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-    IPV4 ip = {120, 12, 3, 10};
-    afficher_ipv4(&ip);
-    MAC mac = {225, 128, 12, 46, 0, 1};
-    afficher_mac(&mac);
-
     reseau_local r;
     init_reseau(&r);
     if ( charger_reseau("configs/config2.txt", &r) != ERR_OK )
